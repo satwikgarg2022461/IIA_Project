be2fbff2dc850b41b20b340d4592128366cf36b9
riju-talk <talk.riju@gmail.com>
2024-11-14 13:03:27 +0530

    added

---
+++ middleware/matching_logic/db_satwick.py
@@ -0,0 +1,6 @@
+"""
+The logic works such middleware has a common schema for all the databases.
+it wil ask for the mappings thtough these mapping logics and then it will
+fetch the tables of the records. 
+
+"""
